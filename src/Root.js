d022c6cfc6d2cd305f223f041de5efc5debcd7ee
Siddhesh Sutar <siddheshsutar96@gmail.com>
2023-10-14 11:34:07 +0530

    render base

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,7 @@
-import { Provider } from "react-redux";
-import { store } from "./store";
 import App from "./App";
 
 const Root = () => (
-  <Provider store={store}>
     <App />
-  </Provider>
 );
 
 export default Root;
