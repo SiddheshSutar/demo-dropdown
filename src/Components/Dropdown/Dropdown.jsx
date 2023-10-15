43abbc9fa6391cb65e8a356d428aaff0a2148307
Siddhesh Sutar <siddheshsutar96@gmail.com>
2023-10-15 14:12:37 +0530

    input css

--- src/Components/Dropdown/Dropdown.jsx
+++ src/Components/Dropdown/Dropdown.jsx
@@ -2,8 +2,10 @@ import styles from './dropdown.module.scss'
 import {useState} from 'react'
 
 const Dropdown = ({
+    name='Basic Dropdown',
     value,
-    onChange
+    onChange,
+    placeHolder = '',
 }) => {
 
     const [typedText, setTypedText] = useState('')
@@ -14,8 +16,10 @@ const Dropdown = ({
     return (
         <div className={styles['container']}>
             <input
+                name={name}
                 value={typedText}
                 onChange={handleTypedTextChange}
+                placeholder={placeHolder}
             />
         </div>
     );
