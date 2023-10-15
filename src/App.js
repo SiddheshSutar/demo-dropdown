ca3ec08320847bd0f889acfe20be9e74803b0c0b
Siddhesh Sutar <siddheshsutar96@gmail.com>
2023-10-15 16:50:08 +0530

    dd wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { useEffect, useState } from "react";
 import Dropdown from "./Components/Dropdown/Dropdown";
 import "./styles.css";
 
 export default function App() {
 
+  const [loading, setLoading] = useState(false)
+  const [list, setList] = useState([])
+
+  /** State created such that the actual value of text input should be in parent,
+   * and dropdown should control it via callback
+   */
+  const [value, setValue] = useState('')
+
+  /** Simulate dynamic dropdown values, coming from some API in real life */
+  useEffect(() => {
+
+    setLoading(true)
+
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(res => res.json())
+      .then(data => {
+        setLoading(false)
+        setList(data.map(item => ({ name: item.name, value: item.name })))
+      })
+      .catch(err => {
+        setLoading(false)
+      })
+
+    setLoading(false)
+  }, [])
+
   return (
     <div className="App">
       <h1>Custom dropdown</h1>
-      <Dropdown />
+      <Dropdown
+        placeHolder="Select user"
+        value={value}
+        onChange={val => setValue(val)}
+        options = {list}
+      />
     </div>
   );
 }
--- src/Components/Dropdown/Dropdown.jsx
+++ src/Components/Dropdown/Dropdown.jsx
@@ -1,28 +1,73 @@
 import styles from './dropdown.module.scss'
-import {useState} from 'react'
+import { useState } from 'react'
 
 const Dropdown = ({
-    name='Basic Dropdown',
+    name = 'Basic Dropdown',
     value,
     onChange,
     placeHolder = '',
+    loading,
+    options
 }) => {
 
-    const [typedText, setTypedText] = useState('')
+    const [visible, setVisible] = useState(false)
 
     const handleTypedTextChange = (e) => {
-        setTypedText(e.target.value)
+        onChange(e.target.value)
     }
+
     return (
         <div className={styles['container']}>
-            <input
-                name={name}
-                value={typedText}
-                onChange={handleTypedTextChange}
-                placeholder={placeHolder}
-            />
+            <div className={styles['wrapper']}>
+                <div className={styles['input-container']}>
+                    <input
+                        name={name}
+                        value={value}
+                        onChange={handleTypedTextChange}
+                        placeholder={placeHolder}
+                        onFocus={() => setVisible(true)}
+                        onBlur={() => setVisible(false)}
+                    />
+                    <div className={`${styles['right-caret']} ${visible && styles['hover']}`}>
+                        &#10095;
+                    </div>
+                </div>
+                {visible && <div className={styles['options-container']}>
+                    {
+                        loading &&
+                        <div>
+                            <i>
+                                Loading....
+                            </i>
+                        </div>
+
+                    }
+                    {
+                        !loading && options && <>
+                            <div className={styles['list-container']}>
+                                <ul>
+                                    {
+                                        options
+                                            .filter(item => {
+
+                                                if(!item || !item.name || !value) return true
+
+                                                return value && item.name && item.name.toLowerCase().includes(value.toLowerCase())
+                                            })
+                                            .map((optionsObj, index) => (
+                                            <li key={index}>
+                                                {optionsObj.value}
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                        </>
+                    }
+                </div>}
+            </div>
         </div>
     );
 }
- 
+
 export default Dropdown;
